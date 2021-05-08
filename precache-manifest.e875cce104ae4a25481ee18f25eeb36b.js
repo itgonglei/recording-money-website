face1e9d1abb5d362d6aed5d65b18454127aabaa
龚磊 <371129453@qq.com>
2021-05-08 18:14:05 +0800

    deploy

---
+++ precache-manifest.e875cce104ae4a25481ee18f25eeb36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60db137a439f617a19b993e7e1fe6f54",
+    "url": "/recording-money-website/QR.png"
+  },
+  {
+    "revision": "7c52b749143b909331fa",
+    "url": "/recording-money-website/css/app.c88199eb.css"
+  },
+  {
+    "revision": "d5a93e38266fd7b60f142ed1b7c3d911",
+    "url": "/recording-money-website/index.html"
+  },
+  {
+    "revision": "7c52b749143b909331fa",
+    "url": "/recording-money-website/js/app.7caede6d.js"
+  },
+  {
+    "revision": "699bd89d5e626b9b31dc",
+    "url": "/recording-money-website/js/chunk-vendors.ee1bf863.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/recording-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/recording-money-website/robots.txt"
+  }
+]);
